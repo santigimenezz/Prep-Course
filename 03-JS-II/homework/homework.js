7e2db83f8d1eea064283e352ccdf0cbee8fd381f
Santiago Gimenez <Santiagomanuel9>
2022-08-17 13:32:12 -0300

    primer cambio

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -89,7 +89,7 @@ function esPrimo(numero) {
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
-  //Escribe tu código aquí
+  //Escribe tu código aquíd
 
 }
 
